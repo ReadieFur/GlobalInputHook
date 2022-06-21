278f17e44d47db05d30e973f556fc02ec7d828c7
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-06-21 15:55:27 +0100

    Misc changes

--- src/GlobalInputHook.Tests/Program.cs
+++ src/GlobalInputHook.Tests/Program.cs
@@ -2,7 +2,11 @@
 using GlobalInputHook.Tools;
 using Forms = System.Windows.Forms;
 
+//You must provide a unique IPC name for the program here.
+/*You can optinally also provide the path for the hook program here, the binary must however be called `GlobalInputHook.exe`
+By default the program will look in the current working directory for the binary.*/
 HookClientHelper dataHook = HookClientHelper.GetOrCreateInstance("global_input_hook");
+
 dataHook.keyboardEvent += HookClientHelper_KeyboardEvent;
 dataHook.mouseEvent += HookClientHelper_MouseEvent;
 
@@ -12,7 +16,7 @@ void HookClientHelper_KeyboardEvent(SKeyboardEventData keyboardEventData)
         + $" KeyEvent:{Enum.GetName(keyboardEventData.eventType)}"
         + $" KeyCode:{keyboardEventData.keyCode}"
         + $" Key:{(Forms.Keys)keyboardEventData.keyCode}"
-    );
+    ); //Example output: ...KeyEvent:KEY_DOWN KeyCode:75 Key:K
 }
 
 void HookClientHelper_MouseEvent(SMouseEventData mouseEventData)
@@ -21,7 +25,7 @@ void HookClientHelper_MouseEvent(SMouseEventData mouseEventData)
         + $" MouseEvent:{Enum.GetName(mouseEventData.eventType)}"
         + $" X:{mouseEventData.cursorPosition.x}"
         + $" Y:{mouseEventData.cursorPosition.y}"
-    );
+    ); //Example output: ...MouseEvent:MOUSE_MOVE X:674 Y:362
 }
 
 Console.ReadLine();
