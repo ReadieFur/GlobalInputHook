2f1b15915ad7c65f86e8bf53366e3ee1e8266a53
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-06-26 05:25:46 +0100

    Fix callbackOnCollectedDelegate MDA

--- src/GlobalInputHook/HookManager.cs
+++ src/GlobalInputHook/HookManager.cs
@@ -9,12 +9,16 @@ namespace GlobalInputHook
         public IntPtr hookHandle { get; private set; }
         //Try to keep the callback as fast as possible. Ideally extract the hook data and then process on a different thread.
         private HookHelper.HookProc callback;
+        //Required to keep the callback delegate in memory preventing the GC from disposing of the callback.
+        //https://docs.microsoft.com/en-us/dotnet/framework/debug-trace-profile/callbackoncollecteddelegate-mda
+        private HookHelper.HookProc hookProc;
 
         public HookManager(HookHelper.HookProc callback, int hookType, string? library = null)
         {
             this.callback = callback;
-            if (library == null) hookHandle = HookHelper.SetWindowsHookEx(hookType, HookCallback, AppDomain.GetCurrentThreadId());
-            else hookHandle = HookHelper.SetWindowsHookEx(hookType, HookCallback, HookHelper.LoadLibrary("User32"));
+            hookProc = HookCallback;
+            if (library == null) hookHandle = HookHelper.SetWindowsHookEx(hookType, hookProc, AppDomain.GetCurrentThreadId());
+            else hookHandle = HookHelper.SetWindowsHookEx(hookType, hookProc, HookHelper.LoadLibrary("User32"));
             if (hookHandle == IntPtr.Zero) throw new Win32Exception(Marshal.GetLastWin32Error());
         }
 
