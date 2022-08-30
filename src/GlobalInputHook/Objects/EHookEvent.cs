861e53b0721e2da4029df2a13e103a9ca51d5722
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-08-30 16:31:26 +0100

    Update EHookEvent.cs

--- src/GlobalInputHook/Objects/EHookEvent.cs
+++ src/GlobalInputHook/Objects/EHookEvent.cs
@@ -2,15 +2,16 @@
 
 namespace GlobalInputHook.Objects
 {
+    [Flags]
     [Serializable]
     public enum EHookEvent
     {
-        None,
-        KeyboardKeyUp,
-        KeyboardKeyDown,
-        MouseButtonUp,
-        MouseButtonDown,
-        MouseMove
-        //MouseScroll
+        None            = 2 ^ 0,
+        KeyboardKeyUp   = 2 ^ 1,
+        KeyboardKeyDown = 2 ^ 2,
+        MouseButtonUp   = 2 ^ 3,
+        MouseButtonDown = 2 ^ 4,
+        MouseMove       = 2 ^ 5,
+        //MouseScroll   = 2 ^ 6
     }
 }
